85acae2d7eec58949e17f1d136658230d46e09f5
Green_Field <munderjbaly77@gmail.com>
2023-08-05 05:35:15 -0700

    solve easy problem it's called Sort the People

---
+++ Sort the People/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Solution
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+
+        string temp;
+        for (int i = 0; i < heights.Length; i++)
+        {
+            for (int j = i + 1; j < heights.Length; j++)
+            {
+                if (heights[i] < heights[j])
+                {
+                    heights[i] ^= heights[j];
+                    heights[j] ^= heights[i];
+                    heights[i] ^= heights[j];
+
+                    temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+                }
+            }
+        }
+        //return heights.Select(i => i.ToString()).ToArray();
+        return names;
+    }
+
+}
