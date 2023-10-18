e95a512fb0887739e9fa493fceecaff241f8b744
GreenField96 <munderjbaly77@gmail.com>
2023-10-18 19:55:47 +0200

    add Best Time to Buy and Sell Stock II

---
+++ Best Time to Buy and Sell Stock II/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, this is solution for: Best Time to Buy and Sell Stock II \n!");
+        Solution sol001 =  new Solution();
+        //int[] arr = { 1, 2, 3, 4, 5 };
+        int[] arr = {7,1,5,3,6,4};
+
+        int profit = sol001.MaxProfit(arr);
+        Console.WriteLine("profit is > " + profit );
+    }
+}
+
+public class Solution
+{
+    public int MaxProfit(int[] prices)
+    {
+        int l = 0;
+        int profit = 0;
+        for (int r = 1; r < prices.Count(); r++)
+        {
+            if (prices[l] < prices[r])
+            {
+                profit += prices[r] - prices[l];
+            }
+            l++;
+        }
+        return profit;
+    }
+}
