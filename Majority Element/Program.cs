fbbc1f9e3cb762aaa4d45ea2a228fe78746530e8
GreenField96 <munderjbaly77@gmail.com>
2023-10-20 22:16:48 +0200

    solve Majority Element

---
+++ Majority Element/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        /*int[] arr = { 1, 2, 3, 2, 4, 1, 5, 4, 1 };*/
+        int[] arr = { 1 };
+
+        int elementCount = CountDuplicates(arr);
+
+        Console.WriteLine($"Majority Element is : {elementCount}");
+    }
+
+    static int CountDuplicates(int[] nums)
+    {
+        HashSet<int> distinctNumbers = new();
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        int tmp = 0;
+
+        foreach (int number in nums)
+        {
+            if (!distinctNumbers.Add(number))
+            {
+                if (!dict.ContainsKey(number))
+                    dict.Add(number, 0);
+
+                tmp = dict[number];
+                dict[number] =  ++tmp;
+            }
+        }
+        List<int> myList = new List<int>(distinctNumbers);
+        
+        int maxDuplicateNumber = 0;
+        int keyOfMaxDuplicate = myList[0];
+        foreach (var item in dict)
+        {
+            if (maxDuplicateNumber < item.Value)
+            {
+                maxDuplicateNumber = item.Value;
+                keyOfMaxDuplicate = item.Key;
+            }
+        }
+
+        return keyOfMaxDuplicate;
+    }
+}
