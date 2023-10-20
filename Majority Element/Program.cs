ee091ebfe231007396411d62fc3b9427fcbf53f9
GreenField96 <munderjbaly77@gmail.com>
2023-10-20 23:36:25 +0200

    add best Algo for Majority Element problem

--- Majority Element/Program.cs
+++ Majority Element/Program.cs
@@ -1,6 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
 
+/*
+ * Boyer - Moore Voting Algorithm | O(n)
+ * 
+ public class Solution {
+    public int MajorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+        
+        for (int i = 1; i < nums.Length; i++) {
+            if (count == 0) {
+                candidate = nums[i];
+                count = 1;
+            } else if (candidate == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        
+        return candidate;
+    }
+}
+ */
+
 internal class Program
 {
     static void Main(string[] args)
