704e7097f5772afb8582f9628ff3ff0c2f0ed8a7
GreenField96 <munderjbaly77@gmail.com>
2023-11-08 22:58:12 +0100

    add O(N) solution to Intersection of Two Arrays

--- Intersection of Two Arrays/Program.cs
+++ Intersection of Two Arrays/Program.cs
@@ -1,5 +1,18 @@
 ﻿using System.Collections;
 
+/*O(N) solution
+ * 
+ public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        var set1 = nums1.ToHashSet();
+        var set2 = nums2.ToHashSet();
+
+        set1.IntersectWith(set2);
+
+        return set1.ToArray();
+    }
+}
+ */
 internal class Program
 {
     private static void Main(string[] args)
