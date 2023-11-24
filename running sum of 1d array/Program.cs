5c361111b74211c4138d933af263f2065e572956
GreenField96 <munderjbaly77@gmail.com>
2023-11-24 20:42:46 +0100

    add Running Sum of 1d Array

---
+++ running sum of 1d array/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+      
+      int[] nums = { 1, 2, 3, 4 };
+     
+      Solution s1 = new Solution();
+      int[] results =  s1.RunningSum(nums);
+
+      foreach(int result in results){
+           Console.WriteLine(result);
+      }
+    }
+}
+public class Solution
+{
+    public int[] RunningSum(int[] nums)
+    {
+        int sum = 0;
+        int[] numsSum = new int[nums.Count()];
+        for (int i = 0; i < nums.Count(); i++)
+        {
+            sum += nums[i];
+            numsSum[i] = sum;
+        }
+        return numsSum;
+    }
+}
